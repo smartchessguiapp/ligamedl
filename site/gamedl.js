4d4e001b2d7ae0c0b05575aa3b611ac80f750352
smartchessguiapp <smartchessguiapp@gmail.com>
2018-05-07 09:57:57 +0200

    Restructured with custom widgets

--- site/gamedl.js
+++ site/gamedl.js
@@ -1,260 +1,95 @@
 class Section{
-    constructor(kind){
-        this.kind=kind
-    }
+    constructor(displayName, input, checked){        
+        this.include = new CheckBoxOptionInput(displayName,checked);
+        this.input = input;
+    }    
 }
 
-let SECTIONS = {
-    username: new Section("string"),
-    since: new Section("date"),
-    until: new Section("date"),
-    max: new Section("number"),
-    rated: new Section("check"),
-    perfType: new Section("options"),
-    color: new Section("radio"),
-    analyzed: new Section("check"),
-    moves: new Section("check"),
-    tags: new Section("check"),
-    clocks: new Section("check"),
-    evals: new Section("check")
-}
+let colorValues = {
+    white : "White",
+    black : "Black"
+};
 
 let perfTypes={
-    ultraBullet:"Ultra Bullet",
-    bullet:"Bullet",
-    blitz:"Blitz",
-    rapid:"Rapid",
-    classical:"Classical",
-    correspondence:"Correspondence",
-    chess960:"Chess960",
-    crazyhouse:"Crazyhouse",
-    antichess:"Antichess",
-    atomic:"Atomic",
-    horde:"Horde",
-    kingOfTheHill:"King of the Hill",
-    racingKings:"Racing Kings",
-    threeCheck:"Three Check"
+    ultraBullet: new CheckboxOption("Ultra Bullet",true),
+    bullet: new CheckboxOption("Bullet",true),
+    blitz: new CheckboxOption("Blitz",true),
+    rapid: new CheckboxOption("Rapid",true),
+    classical: new CheckboxOption("Classical",true),
+    correspondence: new CheckboxOption("Correspondence",true),
+    chess960: new CheckboxOption("Chess960",true),
+    crazyhouse: new CheckboxOption("Crazyhouse",true),
+    antichess: new CheckboxOption("Antichess",true),
+    atomic: new CheckboxOption("Atomic",true),
+    horde: new CheckboxOption("Horde",true),
+    kingOfTheHill: new CheckboxOption("King of the Hill",true),
+    racingKings: new CheckboxOption("Racing Kings",true),
+    threeCheck: new CheckboxOption("Three Check",true)
 };
 
-let colorValues={
-    white:"White",
-    black:"Black"
-};
-
-function pad(num, size){
-    var s = num+"";
-    while (s.length < size) s = "0" + s;
-    return s;
+let SECTIONS = {
+    username: new Section("Username",new TextInput("thibault"),true),
+    since: new Section("Since",new DateInput("2000-01-01"),false),
+    until: new Section("Until",new DateInput(dateToDateInputStr(new Date())),false),
+    max: new Section("Max",new TextInput("1000"),false),
+    rated: new Section("Rated",new CheckBox(true),false),
+    perfType: new Section("Rating category",new CheckboxOptionsInput(perfTypes),false),
+    color: new Section("Color",new RadioInput("color",colorValues,"white"),false),
+    analyzed: new Section("Analyzed",new CheckBox(true),false),
+    moves: new Section("Moves",new CheckBox(true),false),
+    tags: new Section("Tags",new CheckBox(true),false),
+    clocks: new Section("Clocks",new CheckBox(true),false),
+    evals: new Section("Evals",new CheckBox(true),false)
 }
 
-function dateToDateInputStr(dateObj){
-	let month = dateObj.getUTCMonth() + 1;
-	let day = dateObj.getUTCDate();
-	let year = dateObj.getUTCFullYear();
-	return ""+year+"-"+pad(month,2)+"-"+pad(day,2);
-}
-
-function dateInputStrToDate(dateInputStr){
-	let parts=dateInputStr.split("-");
-	let year=parseInt(parts[0]);
-	let month=parseInt(parts[1]-1);
-	let day=parseInt(parts[2]);
-	return new Date(year,month,day);
-}
-
-function ge(id){
-    return document.getElementById(id)
-}
-
-function ce(tag){
-    return document.createElement(tag)
-}
-
-function cl(text){
-    l=ce("label")
-    l.innerHTML=text
-    return l
-}
-
-function createCheckBox(id,checked){
-    inp=document.createElement("input")
-    inp.setAttribute("type","checkbox")
-    inp.setAttribute("id",id)
-    if(checked) inp.setAttribute("checked","true")
-    return inp
-}
-
-function createDateInput(id,value){
-    inp=document.createElement("input")
-    inp.setAttribute("type","date")
-    inp.setAttribute("id",id)
-    inp.value=value
-    return inp
-}
-
-function createStringInput(id,value){
-    inp=document.createElement("input")
-    inp.setAttribute("type","text")
-    inp.setAttribute("id",id)
-    inp.value=value
-    return inp
-}
-
-function createNumberInput(id,value){
-    inp=document.createElement("input")
-    inp.setAttribute("type","text")
-    inp.setAttribute("id",id)
-    inp.value=value
-    return inp
-}
-
-function createRadioInput(name,id,checked){
-    inp=document.createElement("input")
-    inp.setAttribute("type","radio")
-    inp.setAttribute("name",name)    
-    inp.setAttribute("id",id)    
-    if(checked) inp.setAttribute("checked","true")
-    return inp
-}
-
-function createRadio(name,values,def){
-    let inpdiv=document.createElement("div")    
-    for(let value in values){
-        l=cl(values[value])
-        inpdiv.appendChild(l)
-        radio=createRadioInput(name,value,value==def)
-        inpdiv.appendChild(radio)
-    }
-    return inpdiv
-}
-
-function collectRadio(name,values){
-    for(let value in values){
-        let e=ge(value)
-        if(e.checked) return value
-    }
-}
-
-function createOptionsInput(ids,value){
-    let inpdiv=document.createElement("div")
-    for(let id in ids){
-        checkbox=createCheckBox(id,value)
-        l=cl(ids[id])
-        inpdiv.appendChild(l)
-        inpdiv.appendChild(checkbox)
-    }
-    return inpdiv
-}
-
-function addClass(e,klass){
-    e.setAttribute("class",klass)
-}
-
-function createInput(kind){
-    if(kind=="check"){
-        return createCheckBox()
-    }
-}
-
-function collectOptions(ids){
-    let list=[]
-    for(let id in ids){
-        if(ge(id).checked){
-            list.push(id)
+function sectionsAsUrl(){
+    list = [];
+    for(let key in SECTIONS){
+        if(key!="username"){
+            let section = SECTIONS[key];            
+            if(section.include.checked()){
+                let input = section.input;
+                if(input instanceof DateInput){
+                    list.push(key + "=" + input.timestampMs());
+                }else if(input instanceof TextInput){
+                    list.push(key + "=" + input.v());
+                }else if(input instanceof RadioInput){
+                    list.push(key + "=" + input.selected());
+                }else if(input instanceof CheckBox){
+                    list.push(key + "=" + input.checked());
+                }else if(input instanceof CheckboxOptionsInput){
+                    list.push(key + "=" + input.getAllChecked().join(","));
+                }
+            }
         }
     }
-    return list.join(",")
+    if(list.length == 0) return "";
+    return "?" + list.join("&");
 }
 
-function build(){
-    let sectionse=ge("sections")
-    for(let key in SECTIONS){        
-        section=SECTIONS[key]
-        kind=section.kind
-        sdiv=ce("div")
-        addClass(sdiv,"section")
-        hdiv=ce("div")        
-        hlabel=ce("label")
-        hlabel.innerHTML=key
-        hcheck=createCheckBox(key,key=="username"?true:false)
-        hdiv.appendChild(hlabel)
-        hdiv.appendChild(hcheck)
-        addClass(hdiv,"sectionhead")
-        bdiv=ce("div")        
-        sid="input_"+key
-        if(kind=="check"){
-            bdiv.appendChild(createCheckBox(sid,true))
-        }
-        if(kind=="date"){
-            bdiv.appendChild(createDateInput(sid,key=="since"?"2000-01-01":dateToDateInputStr(new Date())))
-        }
-        if(kind=="string"){
-            bdiv.appendChild(createStringInput(sid,key=="username"?"thibault":""))
-        }
-        if(kind=="number"){
-            bdiv.appendChild(createNumberInput(sid,1000))
-        }
-        if(kind=="options"){
-            bdiv.appendChild(createOptionsInput(perfTypes,true))
-        }
-        if(kind=="radio"){
-            bdiv.appendChild(createRadio("color",colorValues,"white"))
-        }
-        addClass(bdiv,"sectionbody")
-        sdiv.appendChild(hdiv)
-        sdiv.appendChild(bdiv)
-        sectionse.appendChild(sdiv)
-    }
-}
-
-function setPerfs(value){
-    for(let key in perfTypes){        
-        ge(key).checked=value
-    }
+function setPerfs(checked){
+    SECTIONS.perfType.input.setAll(checked);    
 }
 
 function createLink(){
-    le=ge("link")
-    let list=[]
-    for(key in SECTIONS){
-        sid="input_"+key
-        if(ge(key).checked&&(key!="username")){
-            section=SECTIONS[key]
-            kind=section.kind
-            if(kind=="date"){            
-                if(ge(key).checked){
-                    list.push(key+"="+dateInputStrToDate(ge(sid).value).getTime())
-                }
-            }            
-            if(kind=="number"){            
-                if(ge(key).checked){
-                    list.push(key+"="+ge(sid).value)
-                }
-            }
-            if(kind=="check"){            
-                if(ge(key).checked){
-                    list.push(key+"="+ge(sid).checked)
-                }
-            }
-            if(kind=="options"){            
-                if(ge(key).checked){
-                    list.push(key+"="+collectOptions(perfTypes))
-                }
-            }
-            if(kind=="radio"){                            
-                if(ge(key).checked){
-                    list.push(key+"="+collectRadio("color",colorValues))
-                }
-            }
-        }        
+    url="https://lichess.org/games/export/"+SECTIONS.username.input.getText()+sectionsAsUrl();
+    le = ge("link");
+    le.href = url;
+    le.innerHTML = url;
+}
+
+function build(){
+    let sectionse = ge("sections");
+    for(let key in SECTIONS){        
+        let section=SECTIONS[key];
+        sdiv = new Div().ac("section");        
+        hdiv = new Div();        
+        hdiv.ac("sectionhead");        
+        hdiv.a(section.include);        
+        bdiv = new Div().ac("sectionbody").a(section.input);
+        sdiv.aa([hdiv,bdiv]);
+        sectionse.appendChild(sdiv.e);
     }
-    url="https://lichess.org/games/export/"+ge("input_username").value
-    if(list.length>0){
-        url+="?"+list.join("&")
-    }
-    le.href=url
-    le.innerHTML=url
-    ge("linktext").value=url
 }
 
 build()
